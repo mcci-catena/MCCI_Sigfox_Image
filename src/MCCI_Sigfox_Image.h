3469ddfd826be020b9d130a45a08dee331b4aba3
Terry Moore <tmm@mcci.com>
2020-09-15 23:05:52 -0400

    Add header file

---
+++ src/MCCI_Sigfox_Image.h
@@ -0,0 +1,40 @@
+/*
+
+Module:	MCCI_Sigfox_Image.h
+
+Function:
+	Header file for Sigfox / ST SX1276 binary libararies for Arduino.
+
+Copyright and License:
+	This file copyright (C) 2020 by
+
+		MCCI Corporation
+		3520 Krums Corners Road
+		Ithaca, NY  14850
+
+	See accompanying LICENSE file for copyright and license information.
+
+Author:
+	fullname, MCCI Corporation	September 2020
+
+*/
+
+#ifndef _MCCI_Sigfox_Image_h_
+#define _MCCI_Sigfox_Image_h_	/* prevent multiple includes */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// We declare here the external references or function entry points
+// that will cause the libraries to be pulled in. Generally we try
+// to keep the image library as thin as possible, but it really depends
+// on the contents.
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _MCCI_Sigfox_Image_h_ */
